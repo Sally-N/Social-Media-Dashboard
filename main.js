a7799edb28f623e0a453d23f687b0a45996b099e
Sally-N <snzungula@gmail.com>
2023-01-29 00:38:49 +0300

    funcctionality: dark-mode

--- main.js
+++ main.js
@@ -23,3 +23,55 @@
 //         document.getElementById("press").style.color = "#000";
 //     }
 // }
+const root = document.getElementById('root');
+const slideHousing = document.getElementById('slideHousing');
+console.log(slideHousing);
+
+const allCards = document.querySelectorAll('.card');
+console.log(allCards)
+
+const slideButton = document.getElementById('slider');
+console.log(slideButton);
+
+const texts = document.querySelectorAll('.text');
+console.log(texts);
+
+const darkGreyTexts = document.querySelectorAll('.dark-grey');
+console.log(darkGreyTexts);
+const one = document.getElementById('one');
+const two = document.getElementById('two');
+const three = document.getElementById('three');
+const four = document.getElementById('four');
+
+
+
+function themeToggle() {
+    if (slideButton.style.position = "right") {
+        console.log("light theme on")
+
+        // allCards.style.backgroundColor = "var( --card-background)"
+        texts.forEach(text => text.style.color = "var(--text-color-white)");
+        allCards.forEach(card => { card.style.backgroundColor = "var(--card-background)" });
+        allCards.forEach(card => { card.style.borderColor = "var(--card-background)" });
+        root.style.backgroundColor = "var(--very-dark-blue)";
+
+        slideButton.style.float = "left";
+        slideButton.style.backgroundColor = "var(--very-dark-blue)"
+        slideHousing.style.backgroundColor = "var(--lime-green";
+        darkGreyTexts.forEach(darkGreyText => {
+            darkGreyText.style.color = "var(--text-color)"
+
+        });
+        one.style.borderTopColor = "var(--facebook)";
+        two.style.borderTopColor = "var(--twitter)";
+        three.style.borderTopColor = "var(--instagram)";
+        four.style.borderTopColor = "var(--youtube)";
+    } else {
+
+    }
+
+}
+
+slideHousing.addEventListener("mousemove", themeToggle)
+
+themeToggle()
