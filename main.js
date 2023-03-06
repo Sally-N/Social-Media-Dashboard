7642f0caa401c0445a27e54ae84abdeaf1ea7833
Sally-N <snzungula@gmail.com>
2023-03-06 23:34:59 +0300

    switcher

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
- 'use strict';
+'use strict';
 
 const root = document.getElementById('root');
 const slideHousing = document.getElementById('slideHousing');
-console.log(slideHousing);
 
 const allCards = document.querySelectorAll('.card');
-console.log(allCards)
 
 const slideButton = document.getElementById('slider');
-console.log(slideButton);
 
 const texts = document.querySelectorAll('.text');
 console.log(texts);
 
 const darkGreyTexts = document.querySelectorAll('.dark-grey');
 console.log(darkGreyTexts);
-const one = document.getElementById('one');
+let one = document.getElementById('one');
 const two = document.getElementById('two');
 const three = document.getElementById('three');
 const four = document.getElementById('four');
@@ -23,63 +20,44 @@ const four = document.getElementById('four');
 
 
 function themeToggle() {
-    if (slideButton.classList.contains('slideBtn') ==true) {
-        console.log("light theme on") 
-        console.log(slideButton.classList.contains('slideBtn'))
-        console.log(`checking float ${slideButton.style.float}`) 
-        slideButton.classList.remove("slideBtn") 
-        slideButton.classList.add("slideBtnToLeft")
-        slideHousing.classList.add("sliderDarkTheme")
-        slideHousing.classList.remove("slider");
+    if (slideButton.classList.contains('slideBtnLight') == true) {
+        darkGreyTexts.forEach(
+            darkGreyText => darkGreyText.style.color = "var(--text-color-white)"
+        );
+        slideButton.classList.replace("slideBtnLight", "slideBtnDark")
+        slideHousing.classList.replace("slider", "sliderDarkTheme");
 
-
-        // allCards.style.backgroundColor = "var( --card-background)"
         texts.forEach(text => text.style.color = "var(--text-color-white)");
         allCards.forEach(card => { card.style.backgroundColor = "var(--card-background)" });
         allCards.forEach(card => { card.style.borderColor = "var(--card-background)" });
         root.style.backgroundColor = "var(--very-dark-blue)";
 
-        // slideButton.style.float = "left";
-        // slideButton.style.backgroundColor = "var(--very-dark-blue)";
-        // slideHousing.style.backgroundImage = "var(--dark-theme-toggle)";
-        // darkGreyTexts.forEach(darkGreyText => {
-        //     darkGreyText.style.color = "var(--text-color)"
 
-        // });
         one.style.borderTopColor = "var(--facebook)";
         two.style.borderTopColor = "var(--twitter)";
         three.style.borderTopColor = "var(--instagram)";
         four.style.borderTopColor = "var(--youtube)";
-    } else if (slideButton.classList.contains('slideBtn') == false)  {
-        console.log("sally")
-        
-        console.log(slideButton.classList.contains('slideBtn'))
-        // slideButton.classList.add("slideBtn") 
-        slideButton.classList.add("slideBtn") 
-        slideButton.classList.remove("slideBtnToLeft") 
-        slideHousing.classList.remove("sliderDarkTheme")
-        slideHousing.classList.add("slider");
-        
+    } else {
 
-        // slideButton.className = "slideBtn"
-        // slideButton.style.float = "right";
-        one.style.borderTopColor = "var(--facebook)";
-        two.style.borderTopColor = "var(--twitter)";
+        slideButton.classList.replace("slideBtnDark", "slideBtnLight");
+        slideHousing.classList.replace("sliderDarkTheme", "slider");
+
+        one.style.borderTopColor = "hsl(208, 92%, 53%)"
+ 
         three.style.borderTopColor = "var(--instagram)";
-        four.style.borderTopColor = "var(--youtube)";
-       
+
         slideButton.style.backgroundColor = "var(--text-color-white)"
         // slideHousing.style.backgroundColor = "var(--light-theme-toggle)";
-        darkGreyTexts.forEach(darkGreyText => {
-            darkGreyText.style.color = "black"
-        }); 
-            texts.forEach(text => text.style.color = "var(--very-darkblue-text)");
-       
-            allCards.forEach(card => { card.style.backgroundColor = "var(--grayish-blue-card-background)" });
+        darkGreyTexts.forEach(darkGreyText =>
+            darkGreyText.style.color = "var(--text-color)"
+        );
+        texts.forEach(text => text.style.color = "var(--very-darkblue-text)");
+
+        allCards.forEach(card => { card.style.backgroundColor = "var(--grayish-blue-card-background)" });
         allCards.forEach(card => { card.style.borderColor = "var(--grayish-blue-card-background)" });
         root.style.backgroundColor = "var(--text-color-white)";
 
-        
+
     }
 
 
